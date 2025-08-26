7de9de3efe6f694fa1e35ee2d4058de4cb633bf7
Hammad Ali <hammadali78056@gmail.com>
2025-08-26 10:56:56 +0500

    Initial Commit

---
+++ L1 demo/L1_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_demoApp.swift
+//  L1 demo
+//
+//  Created by Hammad Ali on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct L1_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ L1 demoTests/L1_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  L1_demoTests.swift
+//  L1 demoTests
+//
+//  Created by Hammad Ali on 26/08/2025.
+//
+
+import Testing
+@testable import L1_demo
+
+struct L1_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
